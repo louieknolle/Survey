fd4c22ada8643193732e669ce4f12b42734ef173
Louis Knolle <knollelw@gmail.com>
2022-04-04 20:56:50 -0400

    add read values code in form submit event handler

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  
+  $(#formOne).submit(function(event));
+    event.preventDefault();
+    const nameInput = $('input#name').val();
+    const foodInput = $('input#food').val();
+    const occupationInput = $('input#occupation').val();
+    const pizzaToppingInput = $("#pizza-topping").val();
+    const favseasonInput = $("#fav-season").val();
+    const dogOwnerInput = $("input:radio[name=dog-owner]:checked").val();
+    const dob = $("#born").val();
+    const favoriteColor = $("#color").val();
+
 });
